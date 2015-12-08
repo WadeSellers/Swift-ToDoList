1bed6287c08979f18c4c61e779025fcc567b13de
Wade Sellers <wade@alkemy.io>
2015-12-07 21:50:35 -0700

    Inital Commit

--- To Do List/ViewController.swift
+++ To Do List/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
